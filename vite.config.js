4432033541a4c4841b3bdbfb1bd5cd06b36f209c
Semin A <somin.oleksandr@gmail.com>
2024-11-26 15:39:30 +0200

    Configure vite path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/sominoleksandr.github.io/',
   resolve: {
     alias: [
         {find: "@", replacement: path.resolve(__dirname, 'src')}
